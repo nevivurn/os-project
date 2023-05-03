ec49f4d102fbf769cefa7b6a616209a6e69969fe
Yongun Seong <nevivurn@nevi.dev>
2023-05-04 06:02:00 +0900

    feat: deps and build

--- build-rpi3.sh
+++ build-rpi3.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ############################################
 # OSSPR2022                                #
@@ -14,7 +14,12 @@ type ccache
 # Some cleanups and setups
 rm -f arch/arm64/boot/Image
 rm -f arch/arm64/boot/dts/broadcom/*.dtb
-CROSS_COMPILER='ccache aarch64-linux-gnu-'
+
+if type aarch64-unknown-linux-gnu-gcc >/dev/null 2>&1; then
+	CROSS_COMPILER='ccache aarch64-unknown-linux-gnu-'
+elif type aarch64-linux-gnu-gcc >/dev/null 2>&1; then
+	CROSS_COMPILER='ccache aarch64-linux-gnu-'
+fi
 
 # Build .config
 make ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILER" tizen_bcmrpi3_defconfig
---
+++ nix/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            bison
+            flex
+            ccache
+            qemu
+            pkgsCross.aarch64-multiplatform.stdenv.cc
+          ] ++ pkgs.linux_4_19.nativeBuildInputs;
+        };
+      }
+    );
+}
